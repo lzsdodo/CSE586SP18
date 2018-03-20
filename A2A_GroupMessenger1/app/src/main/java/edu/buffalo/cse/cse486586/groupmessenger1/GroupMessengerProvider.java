ecfbb739d98e7ba472e4d692c7a055cb755c395d
Zachary <zslianggm@gmail.com>
2018-03-20 12:19:18 -0700

    feat: Add assignment2A

---
+++ A2A_GroupMessenger1/app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger1/GroupMessengerProvider.java
@@ -0,0 +1,110 @@
+package edu.buffalo.cse.cse486586.groupmessenger1;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.util.Log;
+
+/**
+ * GroupMessengerProvider is a key-value table. Once again, please note that we do not implement
+ * full support for SQL as a usual ContentProvider does. We re-purpose ContentProvider's interface
+ * to use it as a key-value table.
+ * 
+ * Please read:
+ * 
+ * http://developer.android.com/guide/topics/providers/content-providers.html
+ * http://developer.android.com/reference/android/content/ContentProvider.html
+ * 
+ * before you start to get yourself familiarized with ContentProvider.
+ * 
+ * There are two methods you need to implement---insert() and query(). Others are optional and
+ * will not be tested.
+ * 
+ * @author stevko
+ *
+ */
+public class GroupMessengerProvider extends ContentProvider {
+
+    static final String TAG = GroupMessengerProvider.class.getSimpleName();
+
+    private SQLiteHelper dbHelper;
+    private SQLiteDatabase db;
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        // You do not need to implement this.
+        return 0;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        // You do not need to implement this.
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        /*
+         * TODO: You need to implement this method. Note that values will have two columns (a key
+         * column and a value column) and one row that contains the actual (key, value) pair to be
+         * inserted.
+         * 
+         * For actual storage, you can use any option. If you know how to use SQL, then you can use
+         * SQLite. But this is not a requirement. You can use other storage options, such as the
+         * internal storage option that we used in PA1. If you want to use that option, please
+         * take a look at the code for PA1.
+         */
+
+        db = dbHelper.getWritableDatabase();
+        long newRowId = db.insertWithOnConflict(dbHelper.TABLE_NAME,
+                dbHelper.getNullColumnHack(), values, db.CONFLICT_IGNORE);
+        Log.v("insert", "row=" + newRowId + "\t" + values.toString());
+        return uri;
+    }
+
+    @Override
+    public boolean onCreate() {
+        // If you need to perform any one-time initialization task, please do it here.
+        dbHelper = new SQLiteHelper(getContext());
+        // Clean Table
+        dbHelper.getWritableDatabase().execSQL("DELETE FROM " + dbHelper.TABLE_NAME + ";");
+        return true;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        // You do not need to implement this.
+        return 0;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+                        String sortOrder) {
+        /*
+         * TODO: You need to implement this method. Note that you need to return a Cursor object
+         * with the right format. If the formatting is not correct, then it is not going to work.
+         *
+         * If you use SQLite, whatever is returned from SQLite is a Cursor object. However, you
+         * still need to be careful because the formatting might still be incorrect.
+         *
+         * If you use a file storage option, then it is your job to build a Cursor * object. I
+         * recommend building a MatrixCursor described at:
+         * http://developer.android.com/reference/android/database/MatrixCursor.html
+         */
+
+        db = dbHelper.getReadableDatabase();
+
+        String[] columns = new String[] {dbHelper.COLUMN_NAME_KEY, dbHelper.COLUMN_NAME_VALUE};
+        String newSelecttion = dbHelper.COLUMN_NAME_KEY + "=?";
+        String[] keyToRead = new String[] {selection};
+
+        Cursor c = db.query(dbHelper.TABLE_NAME, columns, newSelecttion, keyToRead,
+                null, null, null, "1");
+        c.moveToFirst();
+
+        Log.v("query", selection);
+        return c;
+    }
+}
