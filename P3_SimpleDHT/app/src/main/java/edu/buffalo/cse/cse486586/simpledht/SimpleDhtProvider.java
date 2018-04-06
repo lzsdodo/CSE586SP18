ea06aebd26b8c0f183a202e2b48ba268f2ad2e3d
Zachary <zslianggm@gmail.com>
2018-04-06 05:30:34 -0400

    feat: Update DB ContentProvider methods

--- P3_SimpleDHT/app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtProvider.java
+++ P3_SimpleDHT/app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtProvider.java
@@ -12,72 +12,125 @@ public class SimpleDhtProvider extends ContentProvider {
     private SQLiteHelper dbHelper;
     private SQLiteDatabase db;
 
-    @Override
-    public boolean onCreate() {
-        dbHelper = new SQLiteHelper(getContext());
-        dbHelper.getWritableDatabase().execSQL("DELETE FROM " + dbHelper.TABLE_NAME + ";"); // Clean Table
-        return true;
-    }
-    
+    // API, with logic control in these functions
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        db = dbHelper.getWritableDatabase();
-
-        long newRowId = db.insertWithOnConflict(dbHelper.TABLE_NAME,
-                dbHelper.getNullColumnHack(), values, db.CONFLICT_IGNORE);
-
-        Log.v("insert", "row=" + newRowId + "\t\'" + values.toString() + "\'");
+        // 1. get key
+        // 2. get kid, genHash(key)
+        // 3. lookup(kid)
+        // 4. insert to local or send to other nodes
+        this.insertOne(values);
         return uri;
     }
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        db = dbHelper.getWritableDatabase();
+        int affectedRows = 0;
 
-        String newSelection = dbHelper.COLUMN_NAME_KEY + "=?";
-        String[] selectedKey = new String[] {selection};
-        int affectedRows = db.delete(dbHelper.TABLE_NAME, newSelection, selectedKey);
+        if (selection.equals("@")) {
+            affectedRows = this.deleteAll();
+        } else if (selection.equals("*")) {
+            affectedRows = this.deleteAll();
+            // Send msg to other nodes
+        } else {
+            // lookup(genHash(key))
+            // Delete local or Send msg to other nodes
+            affectedRows = this.deleteOne(selection);
+        }
 
-        Log.v("delete", "affected rows=" + affectedRows + "\t\'" + selection + "\'");
-        return affectedRows;
-    }
-
-    public int deleteAll() {
-        db = dbHelper.getWritableDatabase();
-
-        int affectedRows = db.delete(dbHelper.TABLE_NAME, null, null);
-
-        Log.v("delete all", "affected rows=" + affectedRows);
         return affectedRows;
     }
 
     // Special string: * / @ as a selection parameter which return all the result for entire DHT or local
     @Override
-    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
-                        String sortOrder) {
-        db = dbHelper.getReadableDatabase();
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        if (selection.equals("@")) {
+            // By local? or other node?
+            return this.queryAll();
+        } else {
+            if (selection.equals("*")) {
+                return this.queryAll();
+            } else {
+                return this.queryOne(selection);
+            }
+        }
+    }
 
+
+    // Basic insert, query and delete operation on database
+    private void insertOne(ContentValues values) {
+        db = dbHelper.getWritableDatabase();
+
+        long newRowId = db.insertWithOnConflict(dbHelper.TABLE_NAME,
+                null, values, db.CONFLICT_IGNORE);
+
+        Log.v("INSERT", "ROW=" + newRowId + "; KV=\'" +
+                values.getAsString("key") + ", " +
+                values.getAsString("value") + "\'");
+
+    }
+
+    private Cursor queryOne(String key) {
         String[] columns = new String[] {dbHelper.COLUMN_NAME_KEY, dbHelper.COLUMN_NAME_VALUE};
-        String newSelection = dbHelper.COLUMN_NAME_KEY + "=?";
-        String[] selectedKey = new String[] {selection};
+        String selection = dbHelper.COLUMN_NAME_KEY + "=?";
+        String[] selectedKey = new String[] {key};
 
-        Cursor c = db.query(dbHelper.TABLE_NAME, columns, newSelection, selectedKey,
+        Cursor c = dbHelper.getReadableDatabase().query(
+                dbHelper.TABLE_NAME, columns, selection, selectedKey,
                 null, null, null, "1");
         c.moveToFirst();
 
-        Log.v("query", selection);
+        Log.v("QUERY", "KV=\'" + key + ", " + c.getString(c.getColumnIndex("value")) + "\'");
         return c;
     }
 
-    public Cursor queryAll() {
-        db = dbHelper.getReadableDatabase();
-
+    private Cursor queryAll() {
         String[] columns = new String[] {dbHelper.COLUMN_NAME_KEY, dbHelper.COLUMN_NAME_VALUE};
-        Cursor c = db.query(dbHelper.TABLE_NAME, columns, null, null,
-                null, null, null, null);
 
-        Log.v("query all", "");
+        Cursor c = dbHelper.getReadableDatabase().query(
+                dbHelper.TABLE_NAME, columns, null, null,
+                null, null, null);
+        c.moveToFirst();
+
+        Log.v("QUERY ALL", c.getCount() + " ROWS.");
         return c;
+    }
+
+    private int deleteOne(String key) {
+        String selection = dbHelper.COLUMN_NAME_KEY + "=?";
+        String[] selectedKey = new String[] {key};
+
+        int affectedRows = dbHelper.getWritableDatabase().delete(
+                dbHelper.TABLE_NAME, selection, selectedKey);
+
+        Log.v("DELETE", "KEY=\'" + key + "\'");
+        return affectedRows;
+    }
+
+    private int deleteAll() {
+        int affectedRows = dbHelper.getWritableDatabase().delete(
+                dbHelper.TABLE_NAME, null, null);
+        Log.v("DELETE ALL", affectedRows + " ROWS.");
+        this.countRows();
+        return affectedRows;
+    }
+
+    private void countRows() {
+        Cursor c = dbHelper.getReadableDatabase().query(
+                dbHelper.TABLE_NAME, null, null, null,
+                null, null, null);
+        Log.v("COUNT", c.getCount() + "");
+        c.close();
+    }
+
+
+    // Other basic functions
+    @Override
+    public boolean onCreate() {
+        dbHelper = new SQLiteHelper(getContext());
+        dbHelper.getWritableDatabase().execSQL("DELETE FROM " + dbHelper.TABLE_NAME + ";"); // Clean Table
+        return true;
     }
 
     @Override
@@ -89,4 +142,5 @@ public class SimpleDhtProvider extends ContentProvider {
     public String getType(Uri uri) {
         return null;
     }
+
 }
