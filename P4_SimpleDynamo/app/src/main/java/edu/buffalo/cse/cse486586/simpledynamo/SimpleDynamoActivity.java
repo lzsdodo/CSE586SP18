d5bcbd838f27bb25e37c7d20d3c50c15f3e3f655
Zachary <zslianggm@gmail.com>
2018-04-18 01:09:59 -0400

    feat: Update database related class

--- P4_SimpleDynamo/app/src/main/java/edu/buffalo/cse/cse486586/simpledynamo/SimpleDynamoActivity.java
+++ P4_SimpleDynamo/app/src/main/java/edu/buffalo/cse/cse486586/simpledynamo/SimpleDynamoActivity.java
@@ -1,22 +1,77 @@
 package edu.buffalo.cse.cse486586.simpledynamo;
 
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.Handler;
+import android.os.Message;
+import android.telephony.TelephonyManager;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.Menu;
 import android.widget.TextView;
 
+/*
+  * Reference:
+ * - Android Dev Docs:
+ *      - Build Your First App: https://developer.android.com/training/basics/firstapp/index.html
+ *      - App Fundamentals: https://developer.android.com/guide/components/fundamentals.html
+ *      - Log: https://developer.android.com/reference/android/util/Log.html
+ *      - UI Overview: https://developer.android.com/guide/topics/ui/overview.html
+ *      - View: https://developer.android.com/reference/android/view/View.html
+ *      - EditText: https://developer.android.com/reference/android/widget/EditText.html
+ *      - TextView: https://developer.android.com/reference/android/widget/TextView.html
+ *      - The Activity Lifecycle: https://developer.android.com/guide/components/activities/activity-lifecycle.html
+ *      - AsyncTask: https://developer.android.com/reference/android/os/AsyncTask.html
+ *      - Handler: https://developer.android.com/reference/android/os/Handler.html
+ *      - Message: https://developer.android.com/reference/android/os/Message.html
+ *      - Button: https://developer.android.com/reference/android/widget/Button.html
+ *      - View.OnClickListener: https://developer.android.com/reference/android/view/View.OnClickListener.html
+ * - Previous Project:
+ *      - GroupMessenger 2B and SimpleDHT
+ */
+
 public class SimpleDynamoActivity extends Activity {
 
-	@Override
+	static final int UI = 0x00;
+	static Handler uiHandler;
+    static int uiCounter = 0;
+
+    public TextView mTextView;
+    public ContentResolver mCR;
+
+	@SuppressLint("HandlerLeak")
+    @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_simple_dynamo);
     
-		TextView tv = (TextView) findViewById(R.id.textView1);
-        tv.setMovementMethod(new ScrollingMovementMethod());
-	}
+		mTextView = (TextView) findViewById(R.id.textView1);
+        mTextView.setMovementMethod(new ScrollingMovementMethod());
+
+        TelephonyManager tel = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+        String MY_PORT = tel.getLine1Number().substring(tel.getLine1Number().length() - 4);
+
+        uiHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                switch (msg.what) {
+                    case UI:
+                        uiCounter++;
+                        if (uiCounter > 30) {
+                            mTextView.setText("CLEAR UI...\n");
+                        }
+                        mTextView.append(msg.obj + "\n");
+                        break;
+                    default: break;
+                }
+            }
+        };
+
+    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -28,6 +83,7 @@ public class SimpleDynamoActivity extends Activity {
 	public void onStop() {
         super.onStop();
 	    Log.v("Test", "onStop()");
+	    // The test will kill process directly, so we can not handle stop on this.
 	}
 
 }
--- P4_SimpleDynamo/app/src/main/java/edu/buffalo/cse/cse486586/simpledynamo/SimpleDynamoProvider.java
+++ P4_SimpleDynamo/app/src/main/java/edu/buffalo/cse/cse486586/simpledynamo/SimpleDynamoProvider.java
@@ -3,59 +3,170 @@ package edu.buffalo.cse.cse486586.simpledynamo;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Formatter;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import android.util.Log;
+
+
+/*
+ * Reference
+ * - Dev Docs:
+ *      - ContentProvider: http://developer.android.com/guide/topics/providers/content-providers.html
+ *      - ContentValues: https://developer.android.com/reference/android/content/ContentValues.html
+ *      - Cursor: https://developer.android.com/reference/android/database/Cursor.html
+ *      - SQLiteOpenHelper: https://developer.android.com/reference/android/database/sqlite/SQLiteOpenHelper.html
+ *      - MatrixCursor: https://developer.android.com/reference/android/database/MatrixCursor.html
+ */
 
 public class SimpleDynamoProvider extends ContentProvider {
 
-	@Override
-	public int delete(Uri uri, String selection, String[] selectionArgs) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+	private SQLiteHelper dbHelper;
 
-	@Override
-	public String getType(Uri uri) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 	@Override
 	public Uri insert(Uri uri, ContentValues values) {
-		// TODO Auto-generated method stub
+		// TODO
 		return null;
 	}
 
+
 	@Override
-	public boolean onCreate() {
-		// TODO Auto-generated method stub
-		return false;
+	public int delete(Uri uri, String selection, String[] selectionArgs) {
+		// TODO
+		return 0;
 	}
 
 	@Override
 	public Cursor query(Uri uri, String[] projection, String selection,
 			String[] selectionArgs, String sortOrder) {
-		// TODO Auto-generated method stub
+		// TODO
+		return null;
+	}
+
+
+    // Basic insert, query and delete operation on database
+    private void insertOne(ContentValues values) {
+        long newRowId = this.dbHelper.getWritableDatabase().insertWithOnConflict(
+                this.dbHelper.TABLE_NAME, null, values, SQLiteDatabase.CONFLICT_IGNORE);
+
+        Log.v("INSERT", "ROW=" + newRowId + "; KV=\'" +
+                values.getAsString("key") + ", " +
+                values.getAsString("value") + "\'");
+    }
+
+    private Cursor queryOne(String key) {
+        String[] columns = new String[] {this.dbHelper.COLUMN_NAME_KEY, this.dbHelper.COLUMN_NAME_VALUE};
+        String selection = this.dbHelper.COLUMN_NAME_KEY + "=?";
+        String[] selectedKey = new String[] {key};
+
+        Cursor c = this.dbHelper.getReadableDatabase().query(
+                this.dbHelper.TABLE_NAME, columns, selection, selectedKey,
+                null, null, null, "1");
+        c.moveToFirst();
+
+        Log.v("QUERY", "KV=\'" + key + ", " + c.getString(c.getColumnIndex("value")) + "\'");
+        return c;
+    }
+
+    private Cursor queryAll() {
+        String[] columns = new String[] {this.dbHelper.COLUMN_NAME_KEY, this.dbHelper.COLUMN_NAME_VALUE};
+
+        Cursor c = this.dbHelper.getReadableDatabase().query(
+                this.dbHelper.TABLE_NAME, columns, null, null,
+                null, null, null);
+        c.moveToFirst();
+
+        Log.v("QUERY ALL", c.getCount() + " ROWS.");
+        return c;
+    }
+
+    private int deleteOne(String key) {
+        String selection = this.dbHelper.COLUMN_NAME_KEY + "=?";
+        String[] selectedKey = new String[] {key};
+
+        int affectedRows = this.dbHelper.getWritableDatabase().delete(
+                this.dbHelper.TABLE_NAME, selection, selectedKey);
+
+        Log.v("DELETE", "KEY=\'" + key + "\'");
+        return affectedRows;
+    }
+
+    private int deleteAll() {
+        int affectedRows = this.dbHelper.getWritableDatabase().delete(
+                this.dbHelper.TABLE_NAME, null, null);
+        Log.v("DELETE ALL", affectedRows + " ROWS.");
+        // this.countRows();
+        return affectedRows;
+    }
+
+    private void countRows() {
+        Cursor c = this.dbHelper.getReadableDatabase().query(
+                this.dbHelper.TABLE_NAME, null, null, null,
+                null, null, null);
+        Log.v("COUNT", c.getCount() + "");
+        c.close();
+    }
+
+
+    private Cursor makeCursor(HashMap<String, String> kvMap) {
+        String[] attributes = {"_id", "key", "value"};
+        MatrixCursor mCursor = new MatrixCursor(attributes);
+        for (Entry entry: kvMap.entrySet()) {
+            mCursor.addRow(new Object[] {
+                    R.drawable.ic_launcher, entry.getKey(), entry.getValue()});
+        }
+        return mCursor;
+    }
+
+    private HashMap<String, String> cursorToHashMap(Cursor c) {
+        HashMap<String, String> map = new HashMap<String, String>();
+
+        c.moveToFirst();
+        while (!c.isAfterLast()) {
+            String k = c.getString(c.getColumnIndex("key"));
+            String v = c.getString(c.getColumnIndex("value"));
+            map.put(k, v);
+            c.moveToNext();
+        }
+        c.close();
+
+        return map;
+    }
+
+
+	@Override
+	public boolean onCreate() {
+		this.dbHelper = SQLiteHelper.getInstance(getContext());
+		this.dbHelper.getWritableDatabase().execSQL("DELETE FROM " + dbHelper.TABLE_NAME + ";"); // Clean Table
+		return true;
+	}
+
+	@Override
+	public String getType(Uri uri) {
 		return null;
 	}
 
 	@Override
 	public int update(Uri uri, ContentValues values, String selection,
 			String[] selectionArgs) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
-    private String genHash(String input) throws NoSuchAlgorithmException {
-        MessageDigest sha1 = MessageDigest.getInstance("SHA-1");
-        byte[] sha1Hash = sha1.digest(input.getBytes());
-        Formatter formatter = new Formatter();
-        for (byte b : sha1Hash) {
-            formatter.format("%02x", b);
-        }
-        return formatter.toString();
-    }
+	private String genHash(String input) throws NoSuchAlgorithmException {
+		MessageDigest sha1 = MessageDigest.getInstance("SHA-1");
+		byte[] sha1Hash = sha1.digest(input.getBytes());
+		Formatter formatter = new Formatter();
+		for (byte b : sha1Hash) {
+			formatter.format("%02x", b);
+		}
+		return formatter.toString();
+	}
+
 }
